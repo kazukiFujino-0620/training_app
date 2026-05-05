14fc2f649a04193ac8f4b1c9643b96cb60e7fa17
kazukiFujino <fighterkazu.0807@gmail.com>
2026-05-06 00:10:20 +0900

    微修正

--- src/main/resources/META-INF/com/example/traning/dao/training/TrainingDetailDao/selectByTrainingId.sql
+++ src/main/resources/META-INF/com/example/traning/dao/training/TrainingDetailDao/selectByTrainingId.sql
@@ -1,8 +1,19 @@
 SELECT
-    /*%expand*/*
+    training_details.id, 
+    training_details.training_id,
+    training_details.set_number, 
+    training_details.weight, 
+    training_details.reps, 
+    training_details.is_completed, 
+    training_details.create_datetime, 
+    training_details.updated_datetime, 
+    trainings.menu, 
+    trainings.is_all_completed
 FROM
     training_details
+JOIN
+    trainings ON training_details.training_id = trainings.id
 WHERE
-    training_id = /* trainingId */1
+    training_details.training_id = /* trainingId */1
 ORDER BY
     set_number ASC
